af68f8f9ddacb4a9098fea5914a3abff207d30c5
JoaoGuiMB <juaogui159@gmail.com>
2022-08-17 20:43:08 +0200

    test: add tests todos

--- index.test.js
+++ index.test.js
@@ -1,12 +1,25 @@
 import test from "node:test";
 import assert from "node:assert";
+import { input1, input2, input3 } from "./utils/data.js";
+import {
+  recipeInput1,
+  recipeInput2,
+  recipeInput3,
+} from "./utils/expectedRecipes.js";
 
-function sum(a, b) {
-  return a + b;
-}
+test("calculate recipe", (testCtx) => {
+  testCtx.todo("first input", () => {
+    const generatedRecipe = generateRecipe(input1);
+    assert.strictEqual(generatedRecipe, recipeInput1);
+  });
 
-test("test", (testCtx) => {
-  testCtx.test(() => {
-    assert.strictEqual(sum(2, 2), 4);
+  testCtx.todo("second input", () => {
+    const generatedRecipe = generateRecipe(input2);
+    assert.strictEqual(generatedRecipe, recipeInput2);
+  });
+
+  testCtx.todo("third input", () => {
+    const generatedRecipe = generateRecipe(input3);
+    assert.strictEqual(generatedRecipe, recipeInput3);
   });
 });
