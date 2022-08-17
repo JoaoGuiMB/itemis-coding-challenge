3047ffabf25d76ad50716d65740865fcb30fc39c
JoaoGuiMB <juaogui159@gmail.com>
2022-08-17 20:27:47 +0200

    chore: add expected sample data

---
+++ expectedRecipes.js
@@ -0,0 +1,56 @@
+export const recipeInput1 = {
+  taxes: 1.5,
+  total: 29.83,
+  products: [
+    {
+      name: "book",
+      finalPrice: 12.49,
+    },
+    {
+      name: "music cd",
+      finalPrice: 16.49,
+    },
+    {
+      name: "chocolate bar",
+      finalPrice: 0.85,
+    },
+  ],
+};
+
+export const recipeInput2 = {
+  taxes: 7.65,
+  total: 65.15,
+  products: [
+    {
+      name: "box of chocolates",
+      finalPrice: 10.5,
+    },
+    {
+      name: "bottle of perfume",
+      finalPrice: 54.65,
+    },
+  ],
+};
+
+export const recipeInput3 = {
+  taxes: 6.7,
+  total: 74.68,
+  products: [
+    {
+      name: "bottle of perfume",
+      finalPrice: 32.19,
+    },
+    {
+      name: "bottle of perfume",
+      finalPrice: 20.89,
+    },
+    {
+      name: "packet of headache pills",
+      finalPrice: 9.75,
+    },
+    {
+      name: "imported box of chocolates",
+      finalPrice: 11.85,
+    },
+  ],
+};
