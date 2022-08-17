2482a6203de1d2d3cdcc22c256b91fd127bcbf72
JoaoGuiMB <juaogui159@gmail.com>
2022-08-17 20:16:37 +0200

    chore: add sample data

---
+++ data.js
@@ -0,0 +1,63 @@
+export const input1 = [
+  {
+    name: "book",
+    price: 12.49,
+    isImported: false,
+    type: "book",
+  },
+  {
+    name: "music cd",
+    price: 14.99,
+    isImported: false,
+    type: "music",
+  },
+  {
+    name: "chocolate bar",
+    price: 0.85,
+    isImported: false,
+    type: "food",
+  },
+];
+
+export const input2 = [
+  {
+    name: "box of chocolates",
+    price: 10,
+    isImported: true,
+    type: "food",
+  },
+  {
+    name: "bottle of perfume",
+    price: 47.5,
+    isImported: true,
+    type: "perfume",
+  },
+];
+
+export const input3 = [
+  {
+    name: "bottle of perfume",
+    price: 27.99,
+    isImported: true,
+    type: "perfume",
+  },
+  {
+    name: "bottle of perfume",
+    price: 18.99,
+    isImported: false,
+    type: "perfume",
+  },
+  {
+    name: "packet of headache pills",
+    price: 9.75,
+    isImported: false,
+    type: "medical",
+  },
+
+  {
+    name: "box of chocolates",
+    price: 11.25,
+    isImported: true,
+    type: "medical",
+  },
+];
